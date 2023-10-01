c2aa9d9856a3dbb4c1ccea061e36c9ef88f33495
vncebrmjo <143272935+vncebrmjo@users.noreply.github.com>
2023-10-01 16:12:10 +0000

    add inventory UI and header

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home_page():
     return render_template('home.html')
 
 if  __name__ == "__main__":
